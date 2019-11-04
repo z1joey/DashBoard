8ff838e2ddb5e4be2ddd585c5b60d131affde1fd
Joey Zhang <joey.zhang@tgitech.net>
2019-11-04 15:27:44 +0800

    refactor: encapsulate dashboard

---
+++ Replicator/Replicator/Dashboard.swift
@@ -0,0 +1,21 @@
+//
+//  Dashboard.swift
+//  Replicator
+//
+//  Created by joey on 11/4/19.
+//  Copyright © 2019 TGI Technology. All rights reserved.
+//
+
+import UIKit
+
+class Dashboard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Replicator/Replicator/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  Replicator
+//
+//  Created by joey on 11/4/19.
+//  Copyright © 2019 TGI Technology. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
